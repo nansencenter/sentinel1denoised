0e35b55112bcdc96c59a5d8751ffe0046e0c85eb
jeong1park <orepaku.android@gmail.com>
2018-09-18 16:38:39 +0200

    update code and related scripts for experiments
    Former-commit-id: 46747074ac02958849050f3ac2a6f5d460b307b2

--- experimentalData/extraScaling/analyze_experiment_extraScalingParameters.py
+++ experimentalData/extraScaling/analyze_experiment_extraScalingParameters.py
@@ -6,25 +6,7 @@ from scipy.optimize import curve_fit
 platform = 'S1A'
 #platform = 'S1B'
 
-bins = 1001
-snnr_dB_range = np.array([-5, +5], dtype=np.float)
-snnr_range = np.array([0, +10], dtype=np.float)
-esf_range = np.array([0, +10], dtype=np.float)
-nnsd_range = np.array([0, +10], dtype=np.float)
-
-snnr_dB_range = [ snnr_dB_range[0]-(snnr_dB_range[-1]-snnr_dB_range[0])/(bins-1)/2.,
-                  snnr_dB_range[-1]+(snnr_dB_range[-1]-snnr_dB_range[0])/(bins-1)/2. ]
-snnr_range = [ snnr_range[0]-(snnr_range[-1]-snnr_range[0])/(bins-1)/2.,
-               snnr_range[-1]+(snnr_range[-1]-snnr_range[0])/(bins-1)/2. ]
-esf_range = [ esf_range[0]-(esf_range[-1]-esf_range[0])/(bins-1)/2.,
-              esf_range[-1]+(esf_range[-1]-esf_range[0])/(bins-1)/2. ]
-nnsd_range = [ nnsd_range[0]-(nnsd_range[-1]-nnsd_range[0])/(bins-1)/2.,
-               nnsd_range[-1]+(nnsd_range[-1]-nnsd_range[0])/(bins-1)/2. ]
-
-
-# import data
 npzFilesAll = sorted(glob.glob('%s_EW_GRDM_1SDH_*_extraScaling.npz' % platform))
-# Check quality disclaimer #30 and #31 in https://qc.sentinel1.eo.esa.int/disclaimer/
 npzFiles = []
 for li, npzFile in enumerate(npzFilesAll):
     startDateTime = datetime.datetime.strptime(npzFile.split('/')[-1][17:32], "%Y%m%dT%H%M%S")
@@ -39,106 +21,104 @@ for li, npzFile in enumerate(npzFilesAll):
         continue
     else:
         npzFiles.append(npzFile)
-IPF_version = {'EW%s' % li: [] for li in range(1,6)}
-esf_hist2d = {'EW%s' % li: np.zeros((len(npzFiles),bins,bins), dtype=np.uint16) for li in range(1,6)}
-nnsd_hist2d = {'EW%s' % li: np.zeros((len(npzFiles),bins,bins), dtype=np.uint16) for li in range(1,6)}
-acq_date = {'EW%s' % li: [] for li in range(1,6)}
+npz = np.load(npzFiles[0])
+snnrEdges = npz['snnrEdges']
+nnsdEdges = npz['nnsdEdges']
+dBsnnrEdges = npz['dBsnnrEdges']
+esfEdges = npz['esfEdges']
+windowSizes = npz['windowSizes']
+nBins = len(snnrEdges)-1
+nnsdHist = {'EW%s' % li: np.zeros((len(windowSizes), nBins, nBins), dtype=np.int64)
+            for li in range(1,6)}
+esfHist = {'EW%s' % li: np.zeros((len(windowSizes), nBins, nBins), dtype=np.int64)
+           for li in range(1,6)}
 for li, npzFile in enumerate(npzFiles):
-    print('importing %s' % npzFile)
     npz = np.load(npzFile)
     for iSW in range(1,6):
-        npzSW = npz['EW%s' % iSW].item()
-        esf_hist2d['EW%s' % iSW][li], snnr_dB_edges, esf_edges = np.histogram2d(
-                                    10*np.log10(npzSW['signalPlusNoiseToNoiseRatio']),
-                                    npzSW['extraScalingFactor'],
-                                    bins=bins,
-                                    range=[snnr_dB_range,esf_range])
-        nnsd_hist2d['EW%s' % iSW][li], snnr_edges, nnsd_edges = np.histogram2d(
-                                    npzSW['signalPlusNoiseToNoiseRatio'],
-                                    npzSW['noiseNormalizedStandardDeviation'],
-                                    bins=bins,
-                                    range=[snnr_range,nnsd_range])
-        IPF_version['EW%s' % iSW].append(npz['IPFversion'])
-        acq_date['EW%s' % iSW].append(datetime.datetime.strptime(npzFile.split('_')[4], '%Y%m%dT%H%M%S'))
-
-for iSW in range(1,6):
-    np.savez_compressed(platform+'_extraScalingFactor_EW%s.npz' % iSW,
-                        IPF_version=IPF_version,
-                        acq_date=acq_date,
-                        snnr_dB_edges=snnr_dB_edges,
-                        esf_edges=esf_edges,
-                        esf_hist2d=esf_hist2d['EW%s' % iSW])
-    np.savez_compressed(platform+'_noiseNormalizedStandardDeviation_EW%s.npz' % iSW,
-                        IPF_version=IPF_version,
-                        acq_date=acq_date,
-                        snnr_edges=snnr_edges,
-                        nnsd_edges=nnsd_edges,
-                        nnsd_hist2d=nnsd_hist2d['EW%s' % iSW])
+        nnsdHist['EW%s' % iSW] += np.transpose(
+            npz['noiseNormalizedStandardDeviationHistogram'].item()['EW%s' % iSW], axes=(0,2,1))
+        esfHist['EW%s' % iSW] += np.transpose(
+            npz['extraScalingFactorHistogram'].item()['EW%s' % iSW], axes=(0,2,1))
+np.savez_compressed(platform+'_extraScaling.npz', windowSizes=windowSizes,
+                    snnrEdges=snnrEdges, nnsdEdges=nnsdEdges, nnsdHist=nnsdHist,
+                    dBsnnrEdges=dBsnnrEdges, esfEdges=esfEdges, esfHist=esfHist)
 
 
-
-def model_function(x,a,k,b,v,q,c):
-    return a+( (k-a) / (c+q*np.exp(-b*x))**(1/v) )
-plt.figure(figsize=(14.4,4.8))
-extraScalingParameters = {'EW%s' % li: {} for li in range(1,6)}
-for iSW in range(1,6):
-    npz = np.load(platform + '_extraScalingFactor_EW%s.npz' % iSW)
-    snnr_dB_edges = npz['snnr_dB_edges']
-    esf_edges = npz['esf_edges']
-    esf_hist2d = npz['esf_hist2d']
-    extraScalingParameters['SNNR'] = snnr_dB_edges[:-1] + np.diff(snnr_dB_edges)/2.
-    snnr_dB = np.array(snnr_dB_edges[:-1] + np.diff(snnr_dB_edges)/2., ndmin=2)
-    snnr_dB = np.reshape(np.repeat(snnr_dB, bins, axis=0),bins*bins)
-    esf = np.array(esf_edges[:-1] + np.diff(esf_edges)/2., ndmin=2)
-    esf = np.reshape(np.repeat(esf, bins, axis=1),bins*bins)
-    weight = np.sum(esf_hist2d,axis=0).T
-    plt.subplot(1,5,iSW)
-    plt.imshow(np.ma.masked_where(weight==0,weight), origin='low', interpolation='none', aspect='auto',
-               extent=[snnr_dB_edges[0], snnr_dB_edges[-1], esf_edges[0], esf_edges[-1]] )
-    weight = np.reshape(weight, bins*bins)
-    snnr_dB = snnr_dB[weight!=0]
-    esf = esf[weight!=0]
-    weight = weight[weight!=0]
-    esf[snnr_dB>=3] = 1.0
-    weight[snnr_dB<=-1] = 0
-    popt, pcov = curve_fit( model_function, snnr_dB, esf, sigma=1./weight, p0=[100,1,1,1,0.005,1], maxfev=10000 )
-    fittedCurve = model_function(extraScalingParameters['SNNR'] , *popt)
-    plt.plot(extraScalingParameters['SNNR'], fittedCurve, 'y')
-    plt.axis([snnr_dB_edges[0], snnr_dB_edges[-1], esf_edges[0], esf_edges[-1]])
-    extraScalingParameters['EW%s' % iSW] = fittedCurve - fittedCurve.min() + 1.
-plt.tight_layout()
-plt.tight_layout()
-
-
-plt.figure(figsize=(14.4,4.8))
+npz = np.load(platform+'_extraScaling.npz')
+snnrEdges = npz['snnrEdges']
+nnsdEdges = npz['nnsdEdges']
+windowSizes = npz['windowSizes']
+nBins = len(snnrEdges)-1
+fitValue = {'EW%s' % li:np.zeros(windowSizes.shape) for li in range(1,6)}
+fitRMSE = {'EW%s' % li:np.zeros(windowSizes.shape) for li in range(1,6)}
+modelCoeffs = {'EW%s' % li:[] for li in range(1,6)}
+def modelFunction(x, a, b, c, d, g):
+    return ( (a-d) / ( (1+( (x/c)** b )) **g) ) + d
 noiseVarianceParameters = {'EW%s' % li: {} for li in range(1,6)}
 for iSW in range(1,6):
-    npz = np.load(platform + '_noiseNormalizedStandardDeviation_EW%s.npz' % iSW)
-    snnr_edges = npz['snnr_edges']
-    nnsd_edges = npz['nnsd_edges']
-    nnsd_hist2d = npz['nnsd_hist2d']
-    snnr = np.array(snnr_edges[:-1] + np.diff(snnr_edges)/2., ndmin=2)
-    snnr = np.reshape(np.repeat(snnr, bins, axis=0),bins*bins)
-    nnsd = np.array(nnsd_edges[:-1] + np.diff(nnsd_edges)/2., ndmin=2)
-    nnsd = np.reshape(np.repeat(nnsd, bins, axis=1),bins*bins)
-    weight = np.sum(nnsd_hist2d,axis=0).T
-    plt.subplot(1,5,iSW)
-    plt.imshow(np.ma.masked_where(weight==0,weight), origin='low', interpolation='none', aspect='auto',
-               extent=[snnr_edges[0], snnr_edges[-1], nnsd_edges[0], nnsd_edges[-1]] )
-    weight = np.reshape(weight, bins*bins)
-    snnr = snnr[weight!=0]
-    nnsd = nnsd[weight!=0]
-    weight = weight[weight!=0]
-    fitMask = (snnr >= 1.0) * (snnr <= 2.0)
-    x = snnr[fitMask]
-    y = nnsd[fitMask]
-    w = weight[fitMask]/snnr[fitMask]
-    pfit1 = np.polyfit(x, y, w=w, deg=1, full=True)
-    print(np.polyval(pfit1[0], 1.0),  np.sqrt(np.sum((w/w.sum())*(y-np.polyval(pfit1[0], x))**2))  )
-    pfit2 = np.polyfit(x, y, w=w, deg=2, full=True)
-    print(np.polyval(pfit2[0], 1.0),  np.sqrt(np.sum((w/w.sum())*(y-np.polyval(pfit2[0], x))**2))  )
-    pfit3 = np.polyfit(x, y, w=w, deg=3, full=True)
-    print(np.polyval(pfit3[0], 1.0),  np.sqrt(np.sum((w/w.sum())*(y-np.polyval(pfit3[0], x))**2))  )
-    noiseVarianceParameters['EW%s' % iSW] = np.polyval(pfit1[0], 1.0)
-plt.tight_layout()
-plt.tight_layout()
+    nnsdHist = npz['nnsdHist'].item()['EW%s' % iSW]
+    for li, ws in enumerate(windowSizes):
+        snnr = np.array(snnrEdges[:-1] + np.diff(snnrEdges)/2, ndmin=2)
+        snnr = np.reshape(np.repeat(snnr, nBins, axis=0), nBins**2)
+        nnsd = np.array(nnsdEdges[:-1] + np.diff(nnsdEdges)/2, ndmin=2)
+        nnsd = np.reshape(np.repeat(nnsd, nBins, axis=1), nBins**2)
+        weight = np.copy(nnsdHist[li]).reshape(nBins**2)
+        snnr = snnr[weight!=0]
+        nnsd = nnsd[weight!=0]
+        weight = weight[weight!=0]
+        fitMask = (snnr >= 1.0) * (snnr <= 2.0)
+        x = snnr[fitMask]
+        y = nnsd[fitMask]
+        w = weight[fitMask]/snnr[fitMask]
+        pfit = np.polyfit(x, y, w=w, deg=2, full=True)
+        fitValue['EW%s' % iSW][li] = np.polyval(pfit[0], 1.0)
+        fitRMSE['EW%s' % iSW][li] = np.sqrt(np.sum((w/w.sum())*(y-np.polyval(pfit[0], x))**2))
+    modelCoeffs['EW%s' % iSW], pcov = curve_fit(
+        modelFunction, windowSizes, fitValue['EW%s' % iSW], sigma=fitRMSE['EW%s' % iSW])
+    noiseVarianceParameters['EW%s' % iSW] = modelCoeffs['EW%s' % iSW][3]
+    '''
+    plt.figure()
+    windowSizesInterpolated = np.linspace(windowSizes[0], windowSizes[-1], len(windowSizes)*10)
+    plt.errorbar(windowSizes, fitValue['EW%s' % iSW], yerr=fitRMSE['EW%s' % iSW], fmt='s')
+    plt.plot(windowSizesInterpolated, modelFunction(windowSizesInterpolated, *modelCoeffs['EW%s' % iSW]))
+    '''
+np.savez(platform+'_nnsd_results.npz',
+         windowSizes=windowSizes, fitValue=fitValue, fitRMSE=fitRMSE, modelCoeffs=modelCoeffs)
+
+
+npz = np.load(platform+'_extraScaling.npz')
+dBsnnrEdges = npz['dBsnnrEdges']
+esfEdges = npz['esfEdges']
+windowSizes = npz['windowSizes']
+nBins = len(dBsnnrEdges)-1
+extraScalingParameters = {'EW%s' % li:np.zeros(nBins) for li in range(1,6)}
+extraScalingParameters['SNNR'] = dBsnnrEdges[:-1] + np.diff(dBsnnrEdges)/2.
+fitValue = {'EW%s' % li:np.zeros((len(windowSizes),nBins)) for li in range(1,6)}
+fitRMSE = {'EW%s' % li:np.zeros(windowSizes.shape) for li in range(1,6)}
+modelCoeffs = {'EW%s' % li:[] for li in range(1,6)}
+def modelFunction(x, a, k, b, v, q, c):
+    return a+( (k-a) / (c+q*np.exp(-b*x))**(1/v) )
+for iSW in range(1,6):
+    esfHist = npz['esfHist'].item()['EW%s' % iSW]
+    for li, ws in enumerate(windowSizes):
+        dBsnnr = np.array(dBsnnrEdges[:-1] + np.diff(dBsnnrEdges)/2, ndmin=2)
+        dBsnnr = np.reshape(np.repeat(dBsnnr, nBins, axis=0), nBins**2)
+        esf = np.array(esfEdges[:-1] + np.diff(esfEdges)/2, ndmin=2)
+        esf = np.reshape(np.repeat(esf, nBins, axis=1), nBins**2)
+        weight = np.copy(esfHist[li]).reshape(nBins**2)
+        dBsnnr = dBsnnr[weight!=0]
+        esf = esf[weight!=0]
+        weight = weight[weight!=0]
+        esf[dBsnnr>=3] = 1.0
+        weight[dBsnnr<=-1] = 0
+        fitMask = (weight >= 1)
+        x = dBsnnr[fitMask]
+        y = esf[fitMask]
+        w = 1/weight[fitMask]
+        popt, pcov = curve_fit(modelFunction, x, y, sigma=w, p0=[100,1,1,1,0.005,1], maxfev=10000)
+        fittedCurve = modelFunction(extraScalingParameters['SNNR'] , *popt)
+        fitValue['EW%s' % iSW][li] = fittedCurve - fittedCurve.min() + 1
+        fitRMSE['EW%s' % iSW][li] = np.sqrt(np.sum((w/w.sum())*(y-modelFunction(x, *popt))**2))
+    extraScalingParameters['EW%s' % iSW] = np.sum(
+        fitValue['EW%s' % iSW].T * (1/fitRMSE['EW%s' % iSW]) / sum(1/fitRMSE['EW%s' % iSW]),axis=1)
+np.savez(platform+'_esf_results.npz', extraScalingParameters=extraScalingParameters)
--- experimentalData/extraScaling/run_experiment_extraScalingParameters.py
+++ experimentalData/extraScaling/run_experiment_extraScalingParameters.py
@@ -5,18 +5,17 @@ from sentinel1denoised.S1_TOPS_GRD_NoiseCorrection import Sentinel1Image
 inputPath = '/Volumes/MacOS8TB/Archives/Sentinel-1/NorthAtlanticOcean/'
 outputPath = '/Volumes/MacOS8TB/Process/sentinel1denoised/experimentalData/extraScaling/'
 
-def run_process(zipFile, polarization):
-    outputFilename = zipFile.split('/')[-1].split('.')[0] + '_extraScaling_%s.npz' % polarization
+def run_process(zipFile):
+    outputFilename = zipFile.split('/')[-1].split('.')[0] + '_extraScaling.npz'
     print(outputFilename)
     if os.path.exists(outputPath + outputFilename):
         print('Processed data file already exists.')
     else:
-        Sentinel1Image(zipFile).experiment_extraScaling(polarization, windowSize=25)
+        Sentinel1Image(zipFile).experiment_extraScaling('HV')
         shutil.move(outputFilename, outputPath)
 
-polarization = 'HV'
 zipFilesAll = sorted(glob.glob(inputPath + 'S1?_EW_GRDM_1SDH_*.zip'),reverse=True)
 zipFilesUnprocessed = [z for z in zipFilesAll
     if not os.path.exists(outputPath + z.split('/')[-1].split('.')[0] + '_extraScaling.npz')]
 pool = Pool(2)
-pool.starmap(run_process, zip(zipFilesUnprocessed,[polarization]*len(zipFilesUnprocessed)))
+pool.map(run_process, zipFilesUnprocessed)
