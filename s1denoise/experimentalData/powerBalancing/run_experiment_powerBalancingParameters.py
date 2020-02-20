db0ac03ab20759a713eb0af1793dc1f479ea3e7e
Denis Demchev <denemc@maud.ad.nersc.no>
2020-02-20 14:17:43 +0100

    updated npz files + minor changes in experimentalData files

--- s1denoise/experimentalData/powerBalancing/run_experiment_powerBalancingParameters.py
+++ s1denoise/experimentalData/powerBalancing/run_experiment_powerBalancingParameters.py
@@ -1,9 +1,16 @@
-import os, sys, glob, shutil
+import os
+import sys
+import glob
+import shutil
 from multiprocessing import Pool
-from sentinel1denoised.S1_TOPS_GRD_NoiseCorrection import Sentinel1Image
+from s1denoise import Sentinel1Image
 
-inputPath = '/Volumes/MacOS8TB/Archives/Sentinel-1/NorthAtlanticOcean/'
-outputPath = '/Volumes/MacOS8TB/Process/sentinel1denoised/experimentalData/powerBalancing/'
+# dir path to noise scaling training data
+inputPath = sys.argv[1]
+outputPath = sys.argv[2]
+
+#inputPath = '/Volumes/MacOS8TB/Archives/Sentinel-1/NorthAtlanticOcean/'
+#outputPath = '/Volumes/MacOS8TB/Process/sentinel1denoised/experimentalData/powerBalancing/'
 
 def run_process(zipFile):
     outputFilename = zipFile.split('/')[-1].split('.')[0] + '_powerBalancing.npz'
@@ -14,9 +21,10 @@ def run_process(zipFile):
         Sentinel1Image(zipFile).experiment_powerBalancing('HV')
         shutil.move(outputFilename, outputPath)
 
-zipFilesAll = sorted(glob.glob(inputPath + 'S1?_EW_GRDM_1SDH_*.zip'),reverse=True)
+zipFilesAll = sorted(glob.glob('%s/S1?_EW_GRDM_1SDH_*.zip' % (inputPath)), reverse=True)
 zipFilesUnprocessed = [z for z in zipFilesAll
-    if not os.path.exists(outputPath + z.split('/')[-1].split('.')[0] + '_powerBalancing.npz')]
+    if not os.path.exists(outputPath + os.path.basename(z).split('.')[0] + '_powerBalancing.npz')]
+
 pool = Pool(2)
 pool.map(run_process, zipFilesUnprocessed)
 pool.close(); pool.terminate(); pool.join();
