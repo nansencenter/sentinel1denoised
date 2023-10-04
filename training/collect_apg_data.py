c29a2490515bab389b353d2db9eacabc4cdda59f
akorosov <korosov@gmail.com>
2023-10-04 08:51:49 +0200

    add collect_apg_data.py update_apg_coefficients.py

---
+++ training/collect_apg_data.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+
+""" This python script process individual S1 Level-1 GRD files to get values of antenna pattern gain (APG), sigma0, incidence angle for each noise vector
+
+run example:
+python collect_apg_data.py /path/to/L1/GRD/files /path/to/output/dir
+
+"""
+import argparse
+from collections import defaultdict
+import glob
+import os
+from pathlib import Path
+
+from bs4 import BeautifulSoup
+import numpy as np
+
+from s1denoise import Sentinel1Image
+
+def read_kproc(s1):
+    soup = BeautifulSoup(s1.annotationXML['HV'].toxml())
+    kprop = {spp.swath.text:  float(spp.processorscalingfactor.text)
+             for spp in soup.find_all('swathprocparams')}    
+    return kprop
+
+def get_pgpp_pgpa(s1):
+    soup = BeautifulSoup(s1.annotationXML['HV'].toxml())
+    pgpp = soup.find_all('pgproductphase')
+    pgpa = soup.find_all('pgproductamplitude')
+    pha_dict = defaultdict(list)
+    amp_dict = defaultdict(list)
+
+    for pg in pgpp:
+        pha_dict[pg.parent.parent.parent.swath.text].append(float(pg.text))
+    for pg in pgpa:
+        amp_dict[pg.parent.parent.parent.swath.text].append(float(pg.text))
+    return pha_dict, amp_dict
+
+def get_mean_pgpp_pgpa(s1):
+    pha_dict, amp_dict = get_pgpp_pgpa(s1)
+    pha_dict_avg = {i: np.mean(pha_dict[i]) for i in pha_dict}
+    amp_dict_avg = {i: np.mean(amp_dict[i]) for i in amp_dict}
+    return pha_dict_avg, amp_dict_avg
+
+def parse_run_experiment_args():
+    """ Parse input args for run_experiment_* scripts """
+    parser = argparse.ArgumentParser(description='Process SAFE or ZIP files and collect APG related values')
+    parser.add_argument('-o', '--out-dir', type=Path)
+    parser.add_argument('-f', '--force', action='store_true', help="Force overwrite existing output files")
+    parser.add_argument('-i', '--inp-files', type=Path, nargs='+')
+
+    return parser.parse_args()
+
+def main():
+    polarization = 'HV'
+    args = parse_run_experiment_args()
+    print('Number of files queued:', len(args.inp_files))
+
+    for ifile in args.inp_files:
+        ofile = os.path.join(args.out_dir, os.path.basename(ifile) + '_apg.npz')
+        if os.path.exists(ofile) and not args.force:
+            print(ofile, 'exists')
+            continue
+
+        print(ifile)
+        s1 = Sentinel1Image(str(ifile))
+        try:
+            dn_hv = s1.get_GDALRasterBand('DN_HV').ReadAsArray().astype(float)
+        except RuntimeError:
+            print('GDAL cannot open ', ifile)
+            continue
+
+        line, pixel, noise = s1.get_noise_range_vectors(polarization)
+        swath_ids = s1.get_swath_id_vectors(polarization, line, pixel)
+        eap, rsl = s1.get_eap_rsl_vectors(polarization, line, pixel)
+        ea_interpolator, ia_interpolator = s1.get_elevation_incidence_angle_interpolators(polarization)
+        eleang, incang = s1.get_elevation_incidence_angle_vectors(ea_interpolator, ia_interpolator, line, pixel)
+        cal_s0hv = s1.get_calibration_vectors(polarization, line, pixel)
+        scall_hv = s1.get_noise_azimuth_vectors(polarization, line, pixel)
+
+        dn_hv[dn_hv < 20] = np.nan
+        sigma0hv = s1.get_raw_sigma0_vectors_from_full_size(line, pixel, swath_ids, dn_hv)
+        sigma0hv_p10 = s1.get_raw_sigma0_vectors_from_full_size(line, pixel, swath_ids, dn_hv, avg_func=lambda x: np.nanpercentile(x, 10))
+        sigma0hv_p20 = s1.get_raw_sigma0_vectors_from_full_size(line, pixel, swath_ids, dn_hv, avg_func=lambda x: np.nanpercentile(x, 20))
+
+        pgpp, pgpa = get_pgpp_pgpa(s1)
+        kproc = read_kproc(s1)
+        eap_xml = s1.import_elevationAntennaPattern('HV')
+        ncf = {i: eap_xml[i]['noiseCalibrationFactor'] for i in eap_xml}
+        acc = eap_xml['EW1']['absoluteCalibrationConstant']
+        ipf = s1.IPFversion
+
+        np.savez(
+            ofile, 
+            line=line,
+            pixel=pixel,
+            noise=noise,
+            swath_ids=swath_ids,
+            eap=eap,
+            rsl=rsl,
+            eleang=eleang,
+            incang=incang,
+            cal_s0hv=cal_s0hv,
+            scall_hv=scall_hv,
+            sigma0hv=sigma0hv,
+            sigma0hv_p10=sigma0hv_p10,
+            sigma0hv_p20=sigma0hv_p20,
+            pgpp=pgpp,
+            pgpa=pgpa,
+            kproc=kproc,
+            ncf=ncf,
+            acc=acc,
+            ipf=ipf,
+        )
+
+if __name__ == "__main__":
+    main()
---
+++ training/update_apg_coefficients.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+
+""" This python script collects values of antenna pattern gain (APG), sigma0, incidence angle, etc from pre-processed files.
+Then it computes coefficient B for equation:
+Y = X B
+
+Where Y is sigma0_HV, and X is a mtrix with the following columns:
+[1, incidence_angel, apg_ew1, 1, apg_ew2, 1, apg_ew3, 1, apg_ew4, 1, apg_ew5, 1]
+The first two columns have values for all rows, other columns have values only for correposning sub-swaths, or zeros for other subswaths.
+
+Coefficient B is computed for each combination of platform, polarisation, mode, IPF
+The results are saved in denoising_parameters.json
+
+run example:
+python process_apg_data.py /path/to/APG-files
+
+"""
+import argparse
+from collections import defaultdict
+import glob
+import json
+import os
+from pathlib import Path
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from s1denoise.utils import skip_swath_borders, build_AY_matrix, solve
+from update_parameter_file import safe_load
+
+array_names = ['line',
+    'pixel',
+    'noise',
+    'swath_ids',
+    'eap',
+    'rsl',
+    'eleang',
+    'incang',
+    'cal_s0hv',
+    'scall_hv',
+    'sigma0hv',
+]
+
+item_names = [
+    'pgpp',
+    'pgpa',
+    'kproc',
+    'ncf',
+    'acc',
+    'ipf'
+]
+
+keep_names = ['ipf', 'sigma0hv', 'swath_ids', 'incang']
+
+polarization = 'HV'
+scale_APG = 1e21
+scale_HV = 1000
+s0hv_max = [None, 3.0, 1.3, 1.0, 0.9, 0.8]
+
+def parse_run_experiment_args():
+    """ Parse input args for run_experiment_* scripts """
+    parser = argparse.ArgumentParser(description='Process SAFE or ZIP files and collect APG related values')
+    parser.add_argument('inp_dir', type=Path)
+    parser.add_argument('out_file', type=Path)
+    return parser.parse_args()
+
+
+args = parse_run_experiment_args()
+ifiles = sorted(glob.glob(f'{args.inp_dir}/S1*_apg.npz'))
+
+l = defaultdict(list)
+for ifile in ifiles:
+    print('Read', ifile)
+    ds = np.load(ifile, allow_pickle=True)
+    d = {n: ds[n] for n in array_names}
+    d.update({n: ds[n].item() for n in item_names})
+    sigma0hv = d['sigma0hv'] ** 2 / d['cal_s0hv'] ** 2
+    apg = (1 / d['eap'] / d['rsl']) ** 2 / d['cal_s0hv'] ** 2 * d['scall_hv']
+    l['ipf'].append(d['ipf'])
+    l['apg'].append(apg)
+    l['sigma0hv'].append(sigma0hv)
+    l['swath_ids'].append(d['swath_ids'])
+    l['incang'].append(d['incang'])
+
+ll = defaultdict(list)
+for ipf, apg, sigma0hv, swath_ids, incang, ifile in zip(l['ipf'], l['apg'], l['sigma0hv'], l['swath_ids'], l['incang'], ifiles):
+    print('Build', ifile)
+    name_parts = os.path.basename(ifile).split('_')
+    platform, mode, resolution, pol = name_parts[0], name_parts[1], name_parts[2], name_parts[3]
+    uid = f'{platform}_{mode}_{resolution}_{pol}_APG_{ipf:04.2f}'
+    swath_ids_skip = skip_swath_borders(swath_ids, skip=2)
+    sigma0hv_s = [i * scale_HV for i in sigma0hv]
+    apg_s = [i * scale_APG for i in apg]
+    A, Y = build_AY_matrix(swath_ids_skip, sigma0hv_s, apg_s, incang, s0hv_max)
+    if A is not None:
+        ll['a'].append(A)
+        ll['y'].append(Y)
+        ll['uid'].append(uid)
+
+uids, uid_inverse = np.unique(ll['uid'], return_inverse=True)
+B = {}
+rmsd = {}
+
+for i, uid in enumerate(uids):
+    print('Solve', uid)
+    uid_indices = np.where(uid_inverse == i)[0]
+    A = [ll['a'][uid_idx] for uid_idx in uid_indices]
+    Y = [ll['y'][uid_idx] for uid_idx in uid_indices]
+    A = np.vstack(A)
+    Y = np.vstack(Y)
+    B[uid], rmsd[uid] = solve(A, Y)
+    Yrec = np.dot(A, B[uid])
+    plt.plot(Y.flat, Yrec, 'k.', alpha=0.1)
+    plt.title(uid)
+    plt.savefig(f'{uid}_quality.png')
+    plt.close()
+
+
+p = safe_load(args.out_file)
+for uid in B:
+    print('Save', uid)
+    p[uid] = dict(
+        Y_SCALE = scale_HV,
+        A_SCALE = scale_APG,
+        B = B[uid].tolist(),
+        RMSD = rmsd[uid],
+    )
+    
+with open(args.out_file, "w") as f:
+    json.dump(p, f)
